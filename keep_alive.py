935f61613b9b21cc420e9ca85f2457aba94b26ac
para187 <parajeuri6@gmail.com>
2026-09-01 15:09:52 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,20 +4,16 @@ from flask import Flask
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
-  return "Bot ipo hai 24/7 na inaendelea kufanya kazi!"
-
+    return "Bot ipo hai 24/7 na inaendelea kufanya kazi!"
 
 def run():
-  # Render inatoa PORT kiotomatiki kupitia os.environ
-  port = int(os.environ.get("PORT", 8080))
-  app.run(host='0.0.0.0', port=port)
-
+    # Render inatoa Port kiotomatiki kupitia Variable ya PORT (kawaida 10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.daemon = True
-  t.start()
-  
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
